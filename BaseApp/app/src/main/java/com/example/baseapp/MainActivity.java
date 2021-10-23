89467b603730fffe150a05afbb72366dd663cc32
pup-paw <wldusdl0310@gmail.com>
2021-10-23 17:20:18 +0900

    Practice4_2

--- BaseApp/app/src/main/java/com/example/baseapp/MainActivity.java
+++ BaseApp/app/src/main/java/com/example/baseapp/MainActivity.java
@@ -8,57 +8,23 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
+    Button btn1;
+    Button btn2;
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-
-        button1.setBackgroundColor(Color.GRAY);
-        button2.setBackgroundColor(Color.GREEN);
-        button3.setBackgroundColor(Color.RED);
-        button4.setBackgroundColor(Color.YELLOW);
-
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
-                startActivity(mIntent);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
-                startActivity(mIntent);
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
-                startActivity(mIntent);
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
     }
 }
