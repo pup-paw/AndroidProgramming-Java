fa8fd859bc6c475ec91d6ed5509ea491cc882a5d
pup-paw <wldusdl0310@gmail.com>
2021-10-23 21:47:12 +0900

    안드로이드 사진 보기

--- BaseApp/app/src/main/java/com/example/baseapp/MainActivity.java
+++ BaseApp/app/src/main/java/com/example/baseapp/MainActivity.java
@@ -4,27 +4,106 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
-    Button btn2;
-    TextView textView;
-    EditText editText;
+    Switch startSwitch;
+
+    RadioGroup rg;
+    RadioButton radioBtn1;
+    RadioButton radioBtn2;
+    RadioButton radioBtn3;
+
+    ImageView img;
+
+    Button finishBtn;
+    Button resetBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
+        startSwitch = (Switch) findViewById(R.id.startSwitch);
+
+        rg = (RadioGroup) findViewById(R.id.rg);
+        radioBtn1 = (RadioButton) findViewById(R.id.radioBtn1);
+        radioBtn2 = (RadioButton) findViewById(R.id.radioBtn2);
+        radioBtn3 = (RadioButton) findViewById(R.id.radioBtn3);
+
+        img = (ImageView) findViewById(R.id.img);
+
+        finishBtn = (Button) findViewById(R.id.finishBtn);
+        resetBtn = (Button) findViewById(R.id.resetBtn);
+
+        startSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(startSwitch.isChecked()) {
+                    rg.setVisibility(View.VISIBLE);
+                }
+                else {
+                    rg.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        radioBtn1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(radioBtn1.isChecked()) {
+                    img.setVisibility(View.VISIBLE);
+                    img.setImageResource(R.drawable.androidpie);
+                }
+            }
+        });
+        radioBtn2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(radioBtn2.isChecked()) {
+                    img.setImageResource(R.drawable.androidq);
+                    img.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        radioBtn3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(radioBtn3.isChecked()) {
+                    img.setImageResource(R.drawable.androidr);
+                    img.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startSwitch.setChecked(false);
+                radioBtn1.setChecked(false);
+                radioBtn2.setChecked(false);
+                radioBtn3.setChecked(false);
+                img.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
