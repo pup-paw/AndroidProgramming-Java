109024a310f3c3e7b39b7072473671dca7e154b2
pup-paw <wldusdl0310@gmail.com>
2021-11-09 22:24:28 +0900

    간단 이미지 뷰어(변경)

--- Project8_2/app/src/main/java/com/pupaw/project8_2/MainActivity.java
+++ Project8_2/app/src/main/java/com/pupaw/project8_2/MainActivity.java
@@ -3,17 +3,17 @@ package com.pupaw.project8_2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPrev, btnNext;
+    TextView textView1;
     myPictureView myPicture;
     int curNum=1;
     File[] imageFiles;
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
         btnPrev = (Button) findViewById(R.id.btnPrev);
         btnNext = (Button) findViewById(R.id.btnNext);
+        textView1 = (TextView) findViewById(R.id.textView1);
         myPicture = (myPictureView) findViewById(R.id.myPictureView1);
 
         imageFiles = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures").listFiles();
@@ -37,14 +38,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(curNum <= 1) {
-                    Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
+                    curNum = imageFiles.length;
                 }
-                else {
-                    curNum--;
-                    imageFname = imageFiles[curNum].toString();
-                    myPicture.imagePath=imageFname;
-                    myPicture.invalidate();
-                }
+                curNum--;
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath=imageFname;
+                myPicture.invalidate();
+                textView1.setText(String.valueOf(curNum)+"/5");
             }
         });
 
@@ -52,14 +52,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(curNum >= imageFiles.length-1) {
-                    Toast.makeText(getApplicationContext(), "마지막 그림입니다", Toast.LENGTH_SHORT).show();
+                    curNum = 0;
                 }
-                else {
-                    curNum++;
-                    imageFname = imageFiles[curNum].toString();
-                    myPicture.imagePath=imageFname;
-                    myPicture.invalidate();
-                }
+                curNum++;
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath=imageFname;
+                myPicture.invalidate();
+                textView1.setText(String.valueOf(curNum)+"/5");
             }
         });
     }
