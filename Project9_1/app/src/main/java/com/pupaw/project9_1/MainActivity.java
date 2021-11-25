d91a608b08d1f437daea2d29120b0b034acdc27b
pup-paw <wldusdl0310@gmail.com>
2021-11-25 12:34:46 +0900

    수정된 간단 그림판 앱

--- Project9_1/app/src/main/java/com/pupaw/project9_1/MainActivity.java
+++ Project9_1/app/src/main/java/com/pupaw/project9_1/MainActivity.java
@@ -14,8 +14,9 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    final static int LINE = 1, CIRCLE = 2;
+    final static int LINE = 1, CIRCLE = 2, RECT = 3;
     static int curShape = LINE;
+    static int color = Color.RED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             paint.setAntiAlias(true);
             paint.setStrokeWidth(5);
             paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.RED);
+            paint.setColor(color);
 
             switch (curShape) {
                 case LINE:
@@ -63,6 +64,9 @@ public class MainActivity extends AppCompatActivity {
                 case CIRCLE:
                     int radius = (int) Math.sqrt(Math.pow(stopX - startX, 2) + Math.pow(stopY - startY, 2));
                     canvas.drawCircle(startX, startY, radius, paint);
+                    break;
+                case RECT:
+                    canvas.drawRect(startX, startY, stopX, stopY, paint);
                     break;
             }
         }
@@ -73,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         menu.add(0, 1, 0, "선 그리기");
         menu.add(0, 2, 0, "원 그리기");
+        menu.add(0, 3, 0, "사각형 그리기");
+        Menu subMenu = menu.addSubMenu("색상 변경 >>");
+        subMenu.add(0, 4, 0, "빨강");
+        subMenu.add(0, 5, 0, "초록");
+        subMenu.add(0, 6, 0, "파랑");
+
         return true;
     }
 
@@ -85,6 +95,19 @@ public class MainActivity extends AppCompatActivity {
             case 2:
                 curShape = CIRCLE; //원
                 return true;
+            case 3:
+                curShape = RECT; //사각형
+                return true;
+            case 4:
+                color = Color.RED;
+                return true;
+            case 5:
+                color = Color.GREEN;
+                return true;
+            case 6:
+                color = Color.BLUE;
+                return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
